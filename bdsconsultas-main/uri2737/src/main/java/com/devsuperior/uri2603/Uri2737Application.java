c21872ea032b10c08c36a00e033915cf29ecefe5
luanalimadeveloper <luanalima.developer@gmail.com>
2022-06-14 11:10:48 -0300

    uri 2737

--- bdsconsultas-main/uri2737/src/main/java/com/devsuperior/uri2603/Uri2737Application.java
+++ bdsconsultas-main/uri2737/src/main/java/com/devsuperior/uri2603/Uri2737Application.java
@@ -1,11 +1,16 @@
 package com.devsuperior.uri2603;
 
+import com.devsuperior.uri2603.dto.LawyerMinDTO;
+import com.devsuperior.uri2603.projections.LawyerMinProjection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.devsuperior.uri2603.repositories.LawyerRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @SpringBootApplication
 public class Uri2737Application implements CommandLineRunner {
@@ -19,6 +24,14 @@ public class Uri2737Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+
+		List<LawyerMinProjection> list = repository.search1();
+		List<LawyerMinDTO> result1 = list.stream().map(x -> new LawyerMinDTO(x)).collect(Collectors.toList());
+
+		System.out.println("_____________________ SQL _____________________");
+		for (LawyerMinDTO obj : result1) {
+			System.out.println(obj);
+		}
 		
 
 	}
